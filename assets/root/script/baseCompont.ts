b46a421b07760ccfc952b2807e5416cd973affe7
刘沼铃 <354244489@qq.com>
2019-05-13 20:23:00 +0800

    今天你愉快了吗

---
+++ assets/root/script/baseCompont.ts
@@ -0,0 +1,11 @@
+
+const { ccclass, property } = cc._decorator;
+@ccclass
+export default class loadScene extends cc.Component {
+    lodaRes(){
+
+    }
+    close(){
+        
+    }
+}
--- assets/root/script/loadManage.ts
+++ assets/root/script/loadManage.ts
@@ -2,6 +2,17 @@
  * 管理加载
  */
 export default class loadManage {
+    constructor(){
+        let instantiate = cc.instantiate
+        cc.instantiate.prototype = (prefab: cc.Prefab) => {
+            if (!(prefab instanceof cc.Prefab)) {//保留克隆对象功能
+                return instantiate(prefab)
+            }
+            let newNode:cc.Node = instantiate(prefab)
+            this.instantiateNodePool[prefab.name].push(newNode)
+        }
+    }
+    instantiateNodePool: { [name: string]: cc.Node[] } = {} //记录预制使用
     resourcePool: { [url: string]: resourceOne } = {}//管理已加载资源
     load(url: string, type: cc.SpriteFrame | cc.Prefab, callBack: Function) {
 
@@ -25,7 +36,9 @@ class resourceOne {
     reduceUseCount() {
         this.useCount--
         if (this.useCount <= 0) {
-            
+
         }
     }
 }
+
+
