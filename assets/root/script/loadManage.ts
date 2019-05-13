dc36b1ca413eea660600c00a565a2725a3a6b240
刘沼铃 <354244489@qq.com>
2019-05-13 14:41:05 +0800

    haha

--- assets/root/script/loadManage.ts
+++ assets/root/script/loadManage.ts
@@ -13,13 +13,19 @@ class resourceOne {
     }
     asset: cc.SpriteFrame | cc.Prefab = null
     useCount: number = 0
+    /**
+     * 增加引用次数
+     */
     addUseCount() {
         this.useCount++
     }
+    /**
+     * 减少引用次数，如果没引用了，就清理掉
+     */
     reduceUseCount() {
         this.useCount--
         if (this.useCount <= 0) {
-
+            
         }
     }
 }
